7e3519a9a6452d8a148899f8502bec30a5613f62
GE-Developer <on-line-misha@mail.ru>
2023-03-16 23:21:08 +0400

    Initial Commit

---
+++ TrafficLightHW/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrafficLightHW
+//
+//  Created by M I C H A E L on 16.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
